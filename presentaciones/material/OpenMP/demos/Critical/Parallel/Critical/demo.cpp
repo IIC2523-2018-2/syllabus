372438245566a921c4a1ac55874bacedb15a7795
Gabriel Vidal <gabrielvidals@gmail.com>
2018-10-22 13:16:38 -0300

    material sobre OpenMP

---
+++ presentaciones/material/OpenMP/demos/Critical/Parallel/Critical/demo.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<omp.h>
+#include<unistd.h>
+using namespace std;
+
+int main() {
+  int time_sleep = 1000000;
+
+  #pragma omp parallel num_threads(4)
+  {
+    #pragma omp critical
+    {
+      usleep(time_sleep);
+      cout<<"Thread Id =  " << omp_get_thread_num() << endl;
+    }
+  }
+}
---
+++ presentaciones/material/OpenMP/demos/Pi/ReductionError/demo.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+#include <ctime>
+#include <sys/time.h>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+double iteration(int n, int N){
+  double frac = (double) n / (double) N;
+  double den = 1 + pow(frac, 2);
+  double ret = 4/den;
+  return ret;
+}
+
+int main(){
+  struct timeval start, end;
+  long mtime, seconds, useconds;
+
+  double pi_tongo = 0; long int N = 50000000;
+
+  gettimeofday(&start, NULL);
+
+  #pragma omp parallel for //reduction(+:pi_tongo)
+    for (long int i = 0; i < N; i++){
+      pi_tongo += iteration(i, N);
+    }
+
+  pi_tongo = pi_tongo/N;
+
+  cout << "Estimated vs real value of pi:" << endl;
+  cout << setprecision(10) << pi_tongo << endl;
+
+  cout << "3.141592653..." << endl;
+
+
+
+  gettimeofday(&end, NULL);
+
+  seconds  = end.tv_sec  - start.tv_sec;
+  useconds = end.tv_usec - start.tv_usec;
+  mtime = ((seconds) * 1000 + useconds/1000.0) + 0.5;
+
+  printf("Elapsed time: %ld milliseconds\n", mtime);
+
+  return 0;
+}
